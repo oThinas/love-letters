03dfd8fca6673f2f2f2c440d66e952d23f771062
oThinas <thiagomartinsprado@gmail.com>
2023-05-11 16:10:28 -0300

    feat: título do documento é exibido

--- public/document.js
+++ public/document.js
@@ -1,5 +1,10 @@
 import { emitText } from './socket.front.js';
 
+const documentName = new URLSearchParams(window.location.search).get('name');
+const documentTitleElement = document.querySelector('#document-title');
+
+documentTitleElement.textContent = documentName || 'Documento sem título';
+
 const textareaElement = document.querySelector('#text-editor');
 textareaElement.addEventListener('keyup', () => {
   emitText(textareaElement.value);
